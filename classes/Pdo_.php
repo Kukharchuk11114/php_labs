bd99beecd53a847294a62f75202e6d1ddea72f80
Illia Kukharchuk <kukharchukillia739@gmail.com>
2023-06-10 17:07:48 +0200

    Lab5

--- classes/Pdo_.php
+++ classes/Pdo_.php
@@ -1,56 +1,130 @@
 <?php
+require './htmlpurifier-4.15.0/library/HTMLPurifier.auto.php';
+include_once 'classes/Aes.php';
 
-$host = 'localhost';
-$db   = 'php';
-$user = 'Illia';
-$pass = 'Php2211.';
-$charset = 'utf8';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$opt = [
-    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-    \PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$pdo = new \PDO($dsn, $user, $pass, $opt);
-class Pdo_ {
-    private $conn;
-
-// get access to db
-    public function __construct(\PDO $pdo){
-        $this->conn = $pdo;
+class Pdo_
+{
+    private $db;
+    private $purifier;
+    public function __construct()
+    {
+        $config = HTMLPurifier_Config::createDefault();
+        $this->purifier = new HTMLPurifier($config);
+        try {
+            $this->db = new PDO('mysql:host=localhost;dbname=php', 'Illia', 'Php2211.');
+        } catch (PDOException $e) {
+            // add relevant code
+            die();
+        }
     }
 
-    public function getUsers($sql){
-        return $this->conn->query($sql)->fetchAll();
+    public function add_user($login, $email, $password)
+    {
+        //generate salt
+        $salt = random_bytes(16);
+        $password = Aes::encrypt($password);
+
+        //hash password with salt
+        $password = hash('sha512', $password . $salt);
+        $login = $this->purifier->purify($login);
+        $email = $this->purifier->purify($email);
+        try {
+            $sql = "INSERT INTO `user`( `login`, `email`, `hash`, `salt`, `id_status`, `password_form`)
+ VALUES (:login,:email,:hash,:salt,:id_status,:password_form)";
+            //hash password
+            $password = hash('sha512', $password);
+            $data = [
+                'login' => $login,
+                'email' => $email,
+                'hash' => $password,
+                'salt' => $salt,
+                'id_status' => '1',
+                'password_form' => '1'
+            ];
+            $this->db->prepare($sql)->execute($data);
+        } catch (Exception $e) {
+            //modify the code here
+            print 'Exception' . $e->getMessage();
+        }
     }
 
-    
-// Dodac wiadomosc
-    public function addMessage($name,$type,$content){
+
+    public function log_user_in($login, $password)
+    {
+        $salt = random_bytes(16);
+        $pepper = "pepper";
+        $sql = "SELECT id,hash,login,salt FROM user
+        WHERE login=:login";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['login' => $login]);
+        $user_data = $stmt->fetch();
+        $password = hash('sha512', $password . $user_data['salt']);
+
+        $login = $this->purifier->purify($login);
+        try {
+            $sql = "SELECT id,hash,login FROM user WHERE login=:login";
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute(['login' => $login]);
+            $user_data = $stmt->fetch();
+            //hash password with salt and pepper
+            if (hash_equals($password, $user_data['hash'])) {
+                echo 'login successfull<BR/>';
+                echo 'You are logged in as: ' . $user_data['login'] . '<BR/>';
+            } else {
+                echo 'login FAILED<BR/>';
+            }
+        } catch (Exception $e) {
+            //modify the code here
+            print 'Exception' . $e->getMessage();
+        }
+    }
+    public function change_pass($login, $password)
+    {
+        $salt = random_bytes(16);
+        $password = hash('sha512', $password . $salt);
+        $login = $this->purifier->purify($login);
+        try
+        {
+            $sql = "UPDATE `user` 
+                    SET `hash`=:hash, `salt`=:salt 
+                    WHERE `login`=:login";
+            $data = [
+                'login' => $login,
+                'hash' => $password,
+                'salt' => $salt,
+            ];
+            $this
+                ->db
+                ->prepare($sql)->execute($data);
+            print 'Success';
+        }
+        catch(Exception $e)
+        {
+            //modify the code here
+            print 'Exception' . $e->getMessage();
+        }
+    }
+    // Dodac wiadomosc
+    public function addMessage($name, $type, $content)
+    {
         var_dump($name, $type, $content);
-        if(!$type)
+        if (!$type)
             return false;
-         $sql = "INSERT INTO message (`name`,`type`, `message`,`deleted`)
-                VALUES ('" . $name . "','" . $type . "','" . $content . "',0)";
+        $sql = "INSERT INTO message (`name`,`type`, `message`,`deleted`)
+            VALUES ('" . $name . "','" . $type . "','" . $content . "',0)";
         echo $sql;
         echo "<br>";
-        return $this->conn->query($sql);
+        return $this->db->query($sql);
     }
-// Edytowac wiadomosc
-     public function editMessage($name,$type,$content, $id){
-         if(!$type){
+    // Edytowac wiadomosc
+    public function editMessage($name, $type, $content, $id)
+    {
+        if (!$type) {
             return false;
-         }
-        $sql = "UPDATE message SET name='".$name. "', type='" .$type. "', message='" .$content. "' WHERE id=" .$id. "";
+        }
+        $sql = "UPDATE message SET name='" . $name . "', type='" . $type . "', message='" . $content . "' WHERE id=" . $id . "";
         echo $sql;
         echo "<br>";
-        return $this->conn->query($sql);
+        return $this->db->query($sql);
     }
-
-     
 }
-
-
-
-?>
--- index.php
+++ index.php
@@ -1,10 +1,135 @@
 <?php
 include_once "classes/Page.php";
+include_once "classes/Pdo_.php";
+
 Page::display_header("Main page");
+
+$Pdo = new Pdo_();
+
+// adding new user
+if (isset($_REQUEST['add_user'])) {
+    $login = $_REQUEST['login'];
+    $email = $_REQUEST['email'];
+    $password = $_REQUEST['password'];
+    $password2 = $_REQUEST['password2'];
+    if ($password == $password2) {
+        $Pdo->add_user($login, $email, $password);
+    } else {
+        echo 'Passwords doesn\'t match';
+    }
+}
+
+// change password
+if (isset($_REQUEST['change_password']))
+{
+    $login = $_REQUEST['loginNewpass'];
+    $password = $_REQUEST['passwordCh'];
+    $password2 = $_REQUEST['passwordCh2'];
+    if ($password == $password2)
+    {
+        $Pdo->change_pass($login, $password);
+    }
+    else
+    {
+        echo 'Passwords doesn\'t match';
+    }
+}
+// LOGINSS new user
+if (isset($_REQUEST['log_user_in'])) {
+    $login = $_REQUEST['login'];
+
+    $password = $_REQUEST['password'];
+    $Pdo->log_user_in($login, $password);
+}
 ?>
 <H2> Main page</H2>
+<!---------------------------------------------------------------------->
+<hr>
+<P> Register new user</P>
+<form method="post" action="index.php">
+    <table>
+        <tr>
+            <td>login</td>
+            <td>
+                <label for="name"></label>
+                <input required type="text" name="login" id="login" size="40" />
+            </td>
+        </tr>
+        <tr>
+            <td>email</td>
+            <td>
+                <label for="name"></label>
+                <input required type="text" name="email" id="email" size="40" />
+            </td>
+        </tr>
+        <tr>
+            <td>password</td>
+            <td>
+                <label for="name"></label>
+                <input required type="text" name="password" id="password" size="40" />
+            </td>
+        </tr>
+        <tr>
+            <td>repeat password</td>
+            <td>
+                <label for="name"></label>
+                <input required type="text" name="password2" id="password2" size="40" />
+            </td>
+        </tr>
+    </table>
+    <input type="submit" id="submit" value="Create account" name="add_user">
+</form>
+<!---------------------------------------------------------------------->
+<hr>
+<P> Log in</P>
+<form method="post" action="index.php">
+    <table>
+        <tr>
+            <td>login</td>
+            <td>
+                <label for="name"></label>
+                <input required type="text" name="login" id="login" size="40" value="" />
+            </td>
+        </tr>
+        <tr>
+            <td>password</td>
+            <td>
+                <label for="name"></label>
+                <input required type="text" name="password" id="password" size="40" value="" />
+            </td>
+        </tr>
+    </table>
+    <input type="submit" id="submit" value="Log in" name="log_user_in">
+</form>
+<!---------------------------------------------------------------------->
+<h3>Change password</h3>
+    <form method="post" action="index.php">
+        <table>
+            <tr>
+                <td>Login</td>
+                <td>
+                    <input type="text" name="login" maxlength="40" required>
+                </td>
+            </tr>
+            <tr>
+                <td>New Password</td>
+                <td>
+                    <input type="password" name="passwordCh" maxlength="255" required>
+                </td>
+            </tr>
+            <tr>
+                <td>Confirm Password</td>
+                <td>
+                    <input type="password" name="passwordCh2" maxlength="255" required>
+                </td>
+            </tr>
+        </table>
+        <input type="submit" name="change_password" value="Change Password">
+    </form>
+    <hr>
 <?php
 Page::display_navigation();
 ?>
- </body>
+</body>
+
 </html>
